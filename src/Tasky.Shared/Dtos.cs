ac3da1f48f4ab2b0699b2b4e535e0d3fc226dcf0
Iury F. A. Ferreira <iury.ferreira@modal.com.br>
2022-08-15 18:05:36 -0300

    :tada: add shared project and dtos

---
+++ src/Tasky.Shared/Dtos.cs
@@ -0,0 +1,8 @@
+﻿namespace Tasky.Shared;
+
+public class Dtos
+{
+    public record AddTaskOnBoardRequestDto(string BoardName, string Text);
+
+    public record AddStepOnTaskRequestDto(string TaskId, string BoardName, string Text);
+}
